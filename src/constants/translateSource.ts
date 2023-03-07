2ddcf1af260f29bd0bbef7b381b73099ebc31cef
Oliver Jägle <oliver.jaegle@deutschebahn.com>
2023-03-07 13:49:03 +0100

    Disable all inbuilt sources

--- src/constants/translateSource.ts
+++ src/constants/translateSource.ts
@@ -15,15 +15,15 @@ export const translateSource: TranslateSource[] = [
     { source: BING_COM, url: 'www.bing.com/translator/' },
     { source: MOJIDICT_COM, url: 'www.mojidict.com' },
     { source: BAIDU_COM, url: 'fanyi.baidu.com' }
-];
+].filter(() => false);
 
 export const audioSource: TranslateSource[] = [
     { source: GOOGLE_COM, url: 'translate.google.com' },
     { source: BING_COM, url: 'www.bing.com/translator/' },
     { source: BAIDU_COM, url: 'fanyi.baidu.com' }
-];
+].filter(() => false);
 
 export const webPageTranslateSource: TranslateSource[] = [
     { source: GOOGLE_COM, url: 'translate.google.com' },
     { source: MICROSOFT_COM, url: 'microsofttranslator.com' }
-];
+].filter(() => false);
